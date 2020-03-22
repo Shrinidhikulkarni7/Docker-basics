eb2d9f23ed938ea56dca42b61fdc83d204c1bbb3
Lalit Adithya <lalitadithya1996@hotmail.com>
2020-03-22 18:16:56 +0530

    Added todo list service and model Scaffolded controller

---
+++ 02 - ASP .NET Core Web API/TodoListWebAPI/TodoListWebAPI/Services/TodoListService.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoListWebAPI.Models;
+
+namespace TodoListWebAPI.Services
+{
+    public class TodoListService
+    {
+        private readonly IList<TodoListItem> todoListItems;
+
+        public TodoListService()
+        {
+            todoListItems = new List<TodoListItem>();
+        }
+
+        public Guid Add(string item)
+        {
+            Guid id = Guid.NewGuid();
+            todoListItems.Add(new TodoListItem
+            {
+                Id = id,
+                Item = item
+            });
+            return id;
+        }
+
+        public TodoListItem Get(Guid id)
+        {
+            return todoListItems.FirstOrDefault(x => x.Id == id);
+        }
+
+        public List<TodoListItem> Get()
+        {
+            return todoListItems.ToList();
+        }
+
+        public void Update(Guid id, string item)
+        {
+            TodoListItem itemInList = todoListItems.FirstOrDefault(x => x.Id == id);
+            if(itemInList == null)
+            {
+                throw new KeyNotFoundException();
+            }
+
+            todoListItems[todoListItems.IndexOf(itemInList)].Item = item;
+        }
+
+        public void Delete(Guid id)
+        {
+            TodoListItem itemInList = todoListItems.FirstOrDefault(x => x.Id == id);
+            if (itemInList == null)
+            {
+                throw new KeyNotFoundException();
+            }
+
+            todoListItems.Remove(itemInList);
+        }
+    }
+}
