adb3bacbb991d5f375cff4f25dc6e5b094080a01
Lalit Adithya <lalitadithya1996@hotmail.com>
2020-03-22 18:18:17 +0530

    Added DI

--- 02 - ASP .NET Core Web API/TodoListWebAPI/TodoListWebAPI/Startup.cs
+++ 02 - ASP .NET Core Web API/TodoListWebAPI/TodoListWebAPI/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using TodoListWebAPI.Services;
 
 namespace TodoListWebAPI
 {
@@ -25,6 +26,7 @@ namespace TodoListWebAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSingleton<ITodoListService, TodoListService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
